295f51b9fdf62bc2df89e980c2a31387d598236c
Artyom Pionov <pionovartem11@gmail.com>
2023-03-23 20:06:34 +0000

    New Commit: Learning about Standard Libraries e.g. Date and Calendar

---
+++ src/Date_and_Calendar/Main.java
@@ -0,0 +1,8 @@
+package Date_and_Calendar;
+
+public class Main {
+    public static void main(String[] args) {
+        // Cool function
+        System.out.println("Milliseconds since 1st of January 1970: " + System.currentTimeMillis());
+    }
+}
